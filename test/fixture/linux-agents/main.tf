19bdd23323ed5f6ba5f91870d109562582402ef2
Julien Corioland <jucoriol@microsoft.com>
2020-06-02 11:42:55 +0200

    add variables in linux-agents fixture

--- test/fixture/linux-agents/main.tf
+++ test/fixture/linux-agents/main.tf
@@ -2,14 +2,14 @@ module "aci-devops-agent" {
   source                  = "../../../"
   enable_vnet_integration = false
   linux_agents_configuration = {
-    agent_name_prefix = "linux-agent"
-    count             = 2,
-    docker_image      = "jcorioland/aci-devops-agent"
-    docker_tag        = "0.2-linux"
+    agent_name_prefix = "linuxagent-${var.random_suffix}"
+    count             = var.agents_count
+    docker_image      = var.agent_docker_image
+    docker_tag        = var.agent_docker_tag
     agent_pool_name   = var.azure_devops_pool_name
   }
-  resource_group_name                = "rg-linux-devops-agents"
-  location                           = "westeurope"
+  resource_group_name                = "rg-terraform-azure-devops-agents-e2e-tests-${var.random_suffix}"
+  location                           = var.location
   azure_devops_org_name              = var.azure_devops_org_name
   azure_devops_personal_access_token = var.azure_devops_personal_access_token
 }

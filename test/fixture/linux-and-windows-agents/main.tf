6dc1310e323e1001b6f2a4d32317dba3783560cc
Julien Corioland <jucoriol@microsoft.com>
2020-06-02 15:45:20 +0200

    fix variables for win+lin test

--- test/fixture/linux-and-windows-agents/main.tf
+++ test/fixture/linux-and-windows-agents/main.tf
@@ -2,25 +2,25 @@ module "aci-devops-agent" {
   source                  = "../../../"
   enable_vnet_integration = false
   linux_agents_configuration = {
-    agent_name_prefix = "linux-agent"
+    agent_name_prefix = "linux-agent-${var.random_suffix}"
     count             = 2,
-    docker_image      = "jcorioland/aci-devops-agent"
-    docker_tag        = "0.2-linux"
+    docker_image      = var.linux_agent_docker_image
+    docker_tag        = var.linux_agent_docker_tag
     agent_pool_name   = var.linux_azure_devops_pool_name
     cpu               = 1
     memory            = 4
   }
   windows_agents_configuration = {
-    agent_name_prefix = "windows-agent"
+    agent_name_prefix = "windows-agent-${var.random_suffix}"
     count             = 2,
-    docker_image      = "jcorioland/aci-devops-agent"
-    docker_tag        = "0.2-win"
+    docker_image      = var.windows_agent_docker_image
+    docker_tag        = var.windows_agent_docker_tag
     agent_pool_name   = var.windows_azure_devops_pool_name
     cpu               = 1
     memory            = 4
   }
-  resource_group_name                = "rg-aci-devops-agents-we"
-  location                           = "westeurope"
+  resource_group_name                = "rg-terraform-azure-devops-agents-e2e-tests-${var.random_suffix}"
+  location                           = var.location
   azure_devops_org_name              = var.azure_devops_org_name
   azure_devops_personal_access_token = var.azure_devops_personal_access_token
 }
--- test/fixture/linux-and-windows-agents/variables.tf
+++ test/fixture/linux-and-windows-agents/variables.tf
@@ -11,9 +11,52 @@ variable azure_devops_personal_access_token {
 variable linux_azure_devops_pool_name {
   type        = string
   description = "The name of the Azure DevOps agent pool in which the linux containerized agents will be deployed (must exist)"
+  default     = "linux-e2e-agents"
 }
 
 variable windows_azure_devops_pool_name {
   type        = string
   description = "The name of the Azure DevOps agent pool in which the windows containerized agents will be deployed (must exist)"
+  default     = "windows-e2e-agents"
+}
+
+variable location {
+  type        = string
+  description = "The Azure location to use"
+  default     = "westeurope"
+}
+
+variable linux_agent_docker_image {
+  type        = string
+  description = "The Docker image to use for the Linux agent"
+  default     = "jcorioland/aci-devops-agent"
+}
+
+variable linux_agent_docker_tag {
+  type        = string
+  description = "The Docker tag to use for the Linux agent"
+  default     = "0.2-linux"
+}
+
+variable windows_agent_docker_image {
+  type        = string
+  description = "The Docker image to use for the Windows agent"
+  default     = "jcorioland/aci-devops-agent"
+}
+
+variable windows_agent_docker_tag {
+  type        = string
+  description = "The Docker tag to use for the Windows agent"
+  default     = "0.2-win"
+}
+
+variable agents_count {
+  type        = number
+  description = "The number of agents to create"
+  default     = 2
+}
+
+variable random_suffix {
+  type        = number
+  description = "A random suffix for resources generated during the test"
 }

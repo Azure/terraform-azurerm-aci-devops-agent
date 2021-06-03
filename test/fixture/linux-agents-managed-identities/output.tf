0de3e37fe073d4e8a0bc54818e45baf993e3c36b
benjguin <bengui@microsoft.com>
2021-06-03 21:01:17 +0200

    module was declared twice

--- test/fixture/linux-agents-managed-identities/output.tf
+++ test/fixture/linux-agents-managed-identities/output.tf
@@ -6,6 +6,3 @@ output "linux_container_group_name" {
   value       = "${module.aci-devops-agent.linux_agents_names[0]}"
   description = "name of the first Linux container group"
 }
-module "aci-devops-agent" {
-    source = "modules/aci-devops-agent"
-}
